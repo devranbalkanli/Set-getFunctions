7e80d81edafe717206ad60d5a7b1011a295c8bf3
User <User@192.168.1.100>
2021-07-14 14:02:42 +0300

    private olan metoda set ve get fonksiyonları uygulayarak özellik verme işlemi

---
+++ Car/src/Car.java
@@ -0,0 +1,44 @@
+
+public class Car {
+
+		private String marka;
+		private String  model;
+		private double motor;
+		private int kapi;
+		
+		
+		public void setMarka(String marka) {
+			this.marka=marka;		
+		}
+
+		public String getMarka() {
+		return this.marka;
+	}
+		
+		public void setModel(String model) {
+			this.model=model;
+		}
+		
+		public String getModel() {
+			return this.model;
+		}
+		
+		public void setMotor(double motor) {
+			this.motor=motor;
+		}
+		
+		public double getMotor() {
+			return this.motor;
+		}
+		
+		public void setKapi(int kapi) {
+			this.kapi=kapi;
+		}
+		
+		public int getKapi() {
+			return this.kapi;
+		}
+	}
+
+	
+
---
+++ Car/src/carTest.java
@@ -0,0 +1,14 @@
+
+public class carTest {
+
+	public static void main(String []args) {
+		
+		Car car1=new Car();
+		
+		car1.setMarka("mercedes");
+		
+		System.out.println("Arabanin markasi \t" + car1.getMarka() );
+		
+		
+	}
+}
